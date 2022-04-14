6ba3f44d3b8b704dfd5e3c038acb4a5d469a7677
afshin <shahinof10009@gmail.com>
2022-04-15 02:17:34 +0430

    add Product and Customer model in store app

--- store/models.py
+++ store/models.py
@@ -1,3 +1,18 @@
+from turtle import title
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255, null=False, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+
+class Customer(models.Model):
+    first_name= models.CharField(max_length=255, null=False)
+    last_name = models.CharField(max_length=255, null=False)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
+
