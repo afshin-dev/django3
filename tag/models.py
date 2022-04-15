57bf09a46f66d83dc4843a88f6e7fa403036a397
afshin <shahinof10009@gmail.com>
2022-04-15 14:11:59 +0430

    adding generic relationship to the tap app for implementing decouple django app

--- tag/models.py
+++ tag/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
 
 # Create your models here.
+class Tag(models.Model):
+    label = models.CharField(max_length=255, null=False, unique=True)
+
+
+class TagItem(models.Model):  # Generic relationship
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    object = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE)  # keep reference to particular object(model)
+
+    object_id = models.PositiveIntegerField() # keep id of particular model
+    content_object = GenericForeignKey() # give us actual object in database 
+    
+
+### for making a Generic relation ship ###
+### we must keep to reference
+# 1- type of object (Product, Video, Post)
+# 2 - id of that specific object (Product(pk=400))
